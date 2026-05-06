bebeb24479ef80edbe805ca9cf6def935d3f87f2
Job Schepens <jobschepens@gmail.com>
2026-05-06 13:18:00 +0200

    sdsd

--- test.sh
+++ test.sh
@@ -7,3 +7,7 @@ git config --global user.email "job.schepens@uni-koeln.de"
 # curl -u "jobschepens" -d "{\"name\":\"rstudiotest2\",\"private\":true}" https://api.github.com/user/repos
 
 git config --global --edit
+
+ls -al ~/.ssh
+
+ssh-keygen -t ed25519 -C "job.schepens@uni-koeln.de"
