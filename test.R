b43c7ef3b99093ac0d77d346e9ccfaaa5e387097
Job Schepens <jobschepens@gmail.com>
2026-05-06 12:41:04 +0200

    Refactor Git configuration commands: remove duplicates from test.R and create test.sh for clarity

--- test.R
+++ test.R
@@ -1,8 +1,6 @@
 ## install if needed (do this exactly once):
 ## install.packages("usethis")
 
-git config --global user.name "Job Schepens"
-git config --global user.email "job.schepens@uni-koeln.de"
 
 library(usethis)
 use_git_config(user.name = "Job Schepens", user.email = "job.schepens@uni-koeln.de")
@@ -23,6 +21,3 @@ chattr_app()
 
 
 
-# winget install --id GitHub.cli
-# gh repo create rstudiotest2 --private --source=. --push
-# curl -u "jobschepens" -d "{\"name\":\"rstudiotest2\",\"private\":true}" https://api.github.com/user/repos
---
+++ test.sh
@@ -0,0 +1,9 @@
+
+git config --global user.name "Job Schepens"
+git config --global user.email "job.schepens@uni-koeln.de"
+
+# winget install --id GitHub.cli
+# gh repo create rstudiotest2 --private --source=. --push
+# curl -u "jobschepens" -d "{\"name\":\"rstudiotest2\",\"private\":true}" https://api.github.com/user/repos
+
+git config --global --edit
